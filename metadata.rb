bc31e157f5a51ef3ef7d92abaf64baace3266c8d
Ernie Brodeur <ebrodeur@ujami.net>
2012-02-24 16:37:41 -0800

    Version bump and updated the metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,14 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures jabberd"
+maintainer       "Ernie Brodeur"
+maintainer_email "ebrodeur@ujami.net"
+license          "Apache2"
+description      "Provides an XMPP server via ejabberd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
+depends          "erlang"
+
+recipe "openvpn", "Installs and configures ejabberd"
+
+attribute "jabber/global/loglevel",
+  :display_name => "The jabber daemon's Loglevel",
+  :description => "0 = none, - 5 = debug",
+  :default => 4
